ea40dd719f1277bdd683abe4f104d71c3ef7b1d4
korkmazthe1 <ilbars.korkmaz@sabanciuniv.edu>
2025-12-15 03:11:55 +0300

    Update SSO configuration and login flow
    Frontend (index.html):
    - Changed SSO buttons to use JavaScript handler instead of direct link
    - Added handleSSOLogin() function that detects environment and constructs correct SSO URL
    - Works for both local development and production server
    
    SimpleSAMLPHP (authsources.php):
    - Updated entityID from apps-local.sabanciuniv.edu to pro2-dev.sabanciuniv.edu/shadowing
    - Now points to correct Azure Entra ID application for the internship portal

--- simplesamlphp-2.4.1/config/authsources.php
+++ simplesamlphp-2.4.1/config/authsources.php
@@ -7,10 +7,10 @@ $config = [
         'saml:SP',
 
         // The entity ID of this SP.
-        'entityID' => 'https://apps-local.sabanciuniv.edu/', // CONFIGURE-ME: EntraID tarafında oluşturulmış olan uygulamnın entityID bilgisi
+        // Must match what's registered in Azure Entra ID
+        'entityID' => 'https://pro2-dev.sabanciuniv.edu/shadowing',
 
         // The entity ID of the IdP this SP should contact.
-        // Can be NULL/unset, in which case the user will be shown a list of available IdPs.
         'idp' => 'https://sts.windows.net/f1a26096-6ac1-45ab-86a3-938aa985bdf5/',
 
     ],
